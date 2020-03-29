3f598ad16166710b9f625fb0706e4e531624a3df
Karanbir Singh <singh981@gmail.com>
2020-03-28 20:27:29 -0400

    implemeted shop page,  collection preview, collection item

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview";
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collections: SHOP_DATA
+        };
+    }
+
+    render() {
+        return (
+            <div className="shop-page">
+                {this.state.collections.map(({id, ...otherCollectionProps}) => {
+                    return (
+                        <CollectionPreview
+                            key={id}
+                            {...otherCollectionProps}
+                        />
+                    );
+                })}
+            </div>
+        );
+    }
+}
+
+export default ShopPage;
